d8f25358ae1ec1fc04a8753deef8be4b0f7b74bc
PheaRun <phearun123@gmail.com>
2026-06-16 11:40:38 +0700

    Fix GitHub Pages base path

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
+    <BrowserRouter basename="/Frontend-SupMaket-Mag-System">
+    <App />
     </BrowserRouter>
   </StrictMode>
 );
